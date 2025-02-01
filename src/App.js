e1ff8ee35b7ad46f0eb55fc2324e682c84bdfe37
Berihuete Nunez, Engel <mastering23@gmail.com>
2025-01-31 19:32:02 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import myComponet from './myComponet';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+ 
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/myComponet.js
@@ -0,0 +1,12 @@
+import react from "react";
+const myComponet = () =>{
+
+return 
+<>
+
+</>
+
+};
+
+
+export default myComponet;
